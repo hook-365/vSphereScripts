1f9342e58bbeef6604001f273d74d0e421d0d388
Anthony Hook <anthony.hook3@gmail.com>
2015-10-23 11:10:00 -0500

    Initial commit
    hello world
    -getCpuReady.ps1
    -README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+README.md
+
+Just a collection of scripts I've either found, created, or modified for vSphere.
+
+Most of these probably assume you're running them from a PowerCLI shell.
---
+++ getCpuReady.ps1
@@ -0,0 +1,34 @@
+# filename getCpuReady.ps1
+# usage .\getCpuReady.ps1 $vCenter $ClusterName #CSVOutput.csv
+# Super informal, assuming you're running this from a PowerCLI shell with modules loaded already
+
+
+Param (
+ $vCenterServer,
+ $clusterName,
+ $CSV
+ )
+
+$intervalseconds = 20 # Realtime CPU stats
+
+$VC = Connect-VIServer $vCenterServer
+
+$vms = Get-Cluster -Name $clusterName | Get-VM | Where {$_.PowerState -eq "PoweredOn"}
+$vmStats = @()
+$vms | % {
+	$summation = $_ | Get-Stat -Stat cpu.ready.summation -MaxSamples 1 -IntervalSecs $intervalSeconds
+
+	$vmName = $_.Name
+	$vmCPU = $_.NumCPU
+	$summation | Where {$_.Instance -eq ""} | % {
+		$instanceStats = New-Object -TypeName PSObject
+		$instanceStats | Add-Member -Name VM_Name -MemberType NoteProperty -Value $vmName
+		$instanceStats | Add-Member -Name CPUReady -MemberType NoteProperty -Value (($_.Value / ($intervalSeconds * 1000)) * 100)
+		$instanceStats | Add-Member -Name Cores -MemberType NoteProperty -Value $vmCPU
+		$instanceStats | Add-Member -Name msDelay -MemberType NoteProperty -Value ("{0:N2}" -f ($_.Value / $intervalseconds))
+		$vmStats += $instanceStats
+	}
+}
+
+$vmStats | Out-GridView
+$vmStats | Export-Csv $CSV -NoTypeInformation -Force
